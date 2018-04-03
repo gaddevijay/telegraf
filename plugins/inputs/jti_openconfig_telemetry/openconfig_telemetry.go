693f112c681d4336d0c6991fd6907a395f8ab5dd
Janish Jindal <janishj@juniper.net>
2018-04-03 10:08:54 +0530

    Added multiple server connections and better logging

--- plugins/inputs/jti_openconfig_telemetry/openconfig_telemetry.go
+++ plugins/inputs/jti_openconfig_telemetry/openconfig_telemetry.go
@@ -20,7 +20,7 @@ import (
 )
 
 type OpenConfigTelemetry struct {
-	Server          string
+	Server          []string
 	Username        string
 	Password        string
 	ClientId        string
@@ -41,7 +41,7 @@ type OpenConfigTelemetry struct {
 
 var sampleConfig = `
   ## Device address to collect telemetry from
-  server = "localhost:1883"
+  server = ["localhost:1883"]
 
   ## Authentication details. Username and password are must if device expects 
   ## authentication. Client ID must be unique when connecting from multiple instances 
@@ -130,201 +130,209 @@ func (m *OpenConfigTelemetry) Start(acc telegraf.Accumulator) error {
 	m.acc = acc
 
 	// Extract device name / IP
-	s := strings.Split(m.Server, ":")
-	grpc_server, grpc_port := s[0], s[1]
-
-	var err error
-	// If a certificate is provided, open a secure channel. Else open insecure one
-	if m.CertFile != "" {
-		creds, err := credentials.NewClientTLSFromFile(m.CertFile, "")
+	for _, server := range m.Server {
+		s := strings.Split(server, ":")
+		grpc_server, grpc_port := s[0], s[1]
+	
+		var err error
+		// If a certificate is provided, open a secure channel. Else open insecure one
+		if m.CertFile != "" {
+			creds, err := credentials.NewClientTLSFromFile(m.CertFile, "")
+			if err != nil {
+				log.Fatalf("E! Failed to read certificate: %v", err)
+			}
+			m.grpcClientConn, err = grpc.Dial(server, grpc.WithTransportCredentials(creds))
+			log.Printf("I! Opened a secure channel to %s on port %s", grpc_server, grpc_port)
+		} else {
+			m.grpcClientConn, err = grpc.Dial(server, grpc.WithInsecure())
+			log.Printf("I! Opened an insecure channel to %s on port %s", grpc_server, grpc_port)
+		}
 		if err != nil {
-			log.Fatalf("E! Failed to read certificate: %v", err)
+			log.Fatalf("E! Failed to connect: %v", err)
 		}
-		m.grpcClientConn, err = grpc.Dial(m.Server, grpc.WithTransportCredentials(creds))
-	} else {
-		m.grpcClientConn, err = grpc.Dial(m.Server, grpc.WithInsecure())
-	}
-	if err != nil {
-		log.Fatalf("E! Failed to connect: %v", err)
-	}
-
-	if m.Debug {
-		log.Printf("I! Opened a new gRPC session to %s on port %s", grpc_server, grpc_port)
-	}
-	// If username, password and clientId are provided, authenticate user before subscribing for data
-	if m.Username != "" && m.Password != "" && m.ClientId != "" {
-		lc := authentication.NewLoginClient(m.grpcClientConn)
-		loginReply, loginErr := lc.LoginCheck(context.Background(), &authentication.LoginRequest{UserName: m.Username, Password: m.Password, ClientId: m.ClientId})
-		if loginErr != nil {
-			log.Fatalf("E! Could not initiate login check: %v", err)
+	
+		if m.Debug {
+			log.Printf("I! Opened a new gRPC session to %s on port %s", grpc_server, grpc_port)
 		}
-
-		// Check if the user is authenticated. Bail if auth error
-		if !loginReply.Result {
-			log.Fatalf("E! Failed to authenticate the user")
+		// If username, password and clientId are provided, authenticate user before subscribing for data
+		if m.Username != "" && m.Password != "" && m.ClientId != "" {
+			lc := authentication.NewLoginClient(m.grpcClientConn)
+			loginReply, loginErr := lc.LoginCheck(context.Background(), &authentication.LoginRequest{UserName: m.Username, Password: m.Password, ClientId: m.ClientId})
+			if loginErr != nil {
+				log.Fatalf("E! Could not initiate login check: %v", err)
+			}
+	
+			// Check if the user is authenticated. Bail if auth error
+			if !loginReply.Result {
+				log.Fatalf("E! Failed to authenticate the user")
+			}
+			log.Printf("I! Successfully authenticated using username, password for device %s", grpc_server)
+		} else {
+			log.Printf("I! Using no authentication for device %s", grpc_server)
 		}
-	}
-	c := telemetry.NewOpenConfigTelemetryClient(m.grpcClientConn)
-	for _, sensor := range m.Sensors {
-		go func(sensor string, acc telegraf.Accumulator) {
-			spathSplit := strings.SplitN(sensor, " ", -1)
-			var sensorName string
-			var pathlist []*telemetry.Path
-			customReportingRate := false
-			var reportingRate uint64
-
-			reportingRate = uint64(m.SampleFrequency)
-
-			if len(spathSplit) > 1 {
-				for i, path := range spathSplit {
-					// We allow custom reporting rate per sensor by specifying rate in milliseconds
-					// followed by measurement name or sensor path
-					if i == 0 {
-						sensorName = path
-						reportingRate, err = strconv.ParseUint(path, 10, 32)
-						if err == nil {
-							customReportingRate = true
-						} else {
-							reportingRate = uint64(m.SampleFrequency)
-							// If the first word is not an integer and starts with /, we treat it as both sensor and a measurement name
-							if strings.HasPrefix(sensorName, "/") {
+		c := telemetry.NewOpenConfigTelemetryClient(m.grpcClientConn)
+		for _, sensor := range m.Sensors {		
+			go func(sensor string, acc telegraf.Accumulator) {
+				spathSplit := strings.SplitN(sensor, " ", -1)
+				var sensorName string
+				var pathlist []*telemetry.Path
+				customReportingRate := false
+				var reportingRate uint64
+	
+				reportingRate = uint64(m.SampleFrequency)
+	
+				if len(spathSplit) > 1 {
+					for i, path := range spathSplit {
+						// We allow custom reporting rate per sensor by specifying rate in milliseconds
+						// followed by measurement name or sensor path
+						if i == 0 {
+							sensorName = path
+							reportingRate, err = strconv.ParseUint(path, 10, 32)
+							if err == nil {
+								customReportingRate = true
+							} else {
+								reportingRate = uint64(m.SampleFrequency)
+								// If the first word is not an integer and starts with /, we treat it as both sensor and a measurement name
+								if strings.HasPrefix(sensorName, "/") {
+									pathlist = append(pathlist, &telemetry.Path{Path: path, SampleFrequency: uint32(reportingRate)})
+								}
+							}
+						} else if customReportingRate && i == 1 {
+							// If our first word is reporting rate for this list of sensors, we treat second word as measurement name and if it has /
+							// we treat it as another sensor to be monitored
+							sensorName = path
+							if strings.HasPrefix(path, "/") {
 								pathlist = append(pathlist, &telemetry.Path{Path: path, SampleFrequency: uint32(reportingRate)})
 							}
-						}
-					} else if customReportingRate && i == 1 {
-						// If our first word is reporting rate for this list of sensors, we treat second word as measurement name and if it has /
-						// we treat it as another sensor to be monitored
-						sensorName = path
-						if strings.HasPrefix(path, "/") {
+						} else {
 							pathlist = append(pathlist, &telemetry.Path{Path: path, SampleFrequency: uint32(reportingRate)})
 						}
-					} else {
-						pathlist = append(pathlist, &telemetry.Path{Path: path, SampleFrequency: uint32(reportingRate)})
 					}
+				} else {
+					sensorName = sensor
+					pathlist = append(pathlist, &telemetry.Path{Path: sensor, SampleFrequency: uint32(reportingRate)})
 				}
-			} else {
-				sensorName = sensor
-				pathlist = append(pathlist, &telemetry.Path{Path: sensor, SampleFrequency: uint32(reportingRate)})
-			}
-
-			for {
-				var r *telemetry.OpenConfigData
-				var errRecv error
-				stream, err := c.TelemetrySubscribe(context.Background(),
-					&telemetry.SubscriptionRequest{PathList: pathlist})
-				if err != nil {
-					log.Fatalf("E! Could not subscribe: %v", err)
-				}
+				log.Printf("Subscribing to %s to device %s on port %s", sensorName, grpc_server, grpc_port)
 				for {
-					r, errRecv = stream.Recv()
-					if (errRecv != nil) {
+					var r *telemetry.OpenConfigData
+					var errRecv error
+					log.Printf("Sending subscription request for %s to device %s on port %s", sensorName, grpc_server, grpc_port)
+					stream, err := c.TelemetrySubscribe(context.Background(),
+						&telemetry.SubscriptionRequest{PathList: pathlist})
+					log.Printf("Subscription Done for %s to device %s on port %s", sensorName, grpc_server, grpc_port)
+					if err != nil {
+						log.Fatalf("E! Could not subscribe: %v", err)
+					}
+					for {
+						r, errRecv = stream.Recv()
+						if (errRecv != nil) {
+							break
+						}
+						// Print incoming data as info if debug is set
+						if m.Debug {
+							log.Printf("I! Received: %v", r)
+						}
+	
+						// Create a point and add to batch
+						tags := make(map[string]string)
+	
+						if errRecv != nil {
+							log.Fatalln("E! Error: %v", err)
+						}
+	
+						// Use empty prefix. We will update this when we iterate over key-value pairs
+						prefix := ""
+	
+						// Insert additional tags
+						tags["device"] = grpc_server
+	
+						dgroups := []DataGroup{}
+	
+						for _, v := range r.Kv {
+							kv := make(map[string]interface{})
+	
+							if v.Key == "__prefix__" {
+								prefix = v.GetStrValue()
+								continue
+							}
+	
+							// Also, lets use prefix if there is one
+							xmlpath, finaltags := spitTagsNPath(prefix + v.Key)
+							finaltags["device"] = grpc_server
+	
+							switch v.Value.(type) {
+							case *telemetry.KeyValue_StrValue:
+								// If StrAsTags is set, we treat all string values as tags
+								if m.StrAsTags {
+									finaltags[xmlpath] = v.GetStrValue()
+								} else {
+									kv[xmlpath] = v.GetStrValue()
+								}
+								break
+							case *telemetry.KeyValue_DoubleValue:
+								kv[xmlpath] = v.GetDoubleValue()
+								break
+							case *telemetry.KeyValue_IntValue:
+								kv[xmlpath] = v.GetIntValue()
+								break
+							case *telemetry.KeyValue_UintValue:
+								kv[xmlpath] = v.GetUintValue()
+								break
+							case *telemetry.KeyValue_SintValue:
+								kv[xmlpath] = v.GetSintValue()
+								break
+							case *telemetry.KeyValue_BoolValue:
+								kv[xmlpath] = v.GetBoolValue()
+								break
+							case *telemetry.KeyValue_BytesValue:
+								kv[xmlpath] = v.GetBytesValue()
+								break
+							}
+	
+							// Insert other tags from message
+							finaltags["_system_id"] = r.SystemId
+							finaltags["_path"] = r.Path
+	
+							// Insert derived key and value
+							dgroups = CollectionByKeys(dgroups).Insert(finaltags, kv)
+	
+							// Insert data from message header
+							dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_sequence": r.SequenceNumber})
+							dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_timestamp": r.Timestamp})
+							dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_component_id": r.ComponentId})
+							dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_subcomponent_id": r.SubComponentId})
+						}
+	
+						// Print final data collection
+						if m.Debug {
+							log.Printf("I! Available collection is: %v", dgroups)
+						}
+	
+						tnow := time.Now()
+						// Iterate through data groups and add them
+						for _, group := range dgroups {
+							if len(group.tags) == 0 {
+								acc.AddFields(sensorName, group.data, tags, tnow)
+							} else {
+								acc.AddFields(sensorName, group.data, group.tags, tnow)
+							}
+						}
+					}
+					if (errRecv == io.EOF){
 						break
 					}
-					// Print incoming data as info if debug is set
-					if m.Debug {
-						log.Printf("I! Received: %v", r)
-					}
-
-					// Create a point and add to batch
-					tags := make(map[string]string)
-
-					if errRecv != nil {
-						log.Fatalln("E! Error: %v", err)
-					}
-
-					// Use empty prefix. We will update this when we iterate over key-value pairs
-					prefix := ""
-
-					// Insert additional tags
-					tags["device"] = grpc_server
-
-					dgroups := []DataGroup{}
-
-					for _, v := range r.Kv {
-						kv := make(map[string]interface{})
-
-						if v.Key == "__prefix__" {
-							prefix = v.GetStrValue()
+					if (errRecv != nil){
+					// Retry only if the retry flag is set to true, else exit with error
+						if m.Retry {
 							continue
-						}
-
-						// Also, lets use prefix if there is one
-						xmlpath, finaltags := spitTagsNPath(prefix + v.Key)
-						finaltags["device"] = grpc_server
-
-						switch v.Value.(type) {
-						case *telemetry.KeyValue_StrValue:
-							// If StrAsTags is set, we treat all string values as tags
-							if m.StrAsTags {
-								finaltags[xmlpath] = v.GetStrValue()
-							} else {
-								kv[xmlpath] = v.GetStrValue()
-							}
-							break
-						case *telemetry.KeyValue_DoubleValue:
-							kv[xmlpath] = v.GetDoubleValue()
-							break
-						case *telemetry.KeyValue_IntValue:
-							kv[xmlpath] = v.GetIntValue()
-							break
-						case *telemetry.KeyValue_UintValue:
-							kv[xmlpath] = v.GetUintValue()
-							break
-						case *telemetry.KeyValue_SintValue:
-							kv[xmlpath] = v.GetSintValue()
-							break
-						case *telemetry.KeyValue_BoolValue:
-							kv[xmlpath] = v.GetBoolValue()
-							break
-						case *telemetry.KeyValue_BytesValue:
-							kv[xmlpath] = v.GetBytesValue()
-							break
-						}
-
-						// Insert other tags from message
-						finaltags["_system_id"] = r.SystemId
-						finaltags["_path"] = r.Path
-
-						// Insert derived key and value
-						dgroups = CollectionByKeys(dgroups).Insert(finaltags, kv)
-
-						// Insert data from message header
-						dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_sequence": r.SequenceNumber})
-						dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_timestamp": r.Timestamp})
-						dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_component_id": r.ComponentId})
-						dgroups = CollectionByKeys(dgroups).Insert(finaltags, map[string]interface{}{"_subcomponent_id": r.SubComponentId})
-					}
-
-					// Print final data collection
-					if m.Debug {
-						log.Printf("I! Available collection is: %v", dgroups)
-					}
-
-					tnow := time.Now()
-					// Iterate through data groups and add them
-					for _, group := range dgroups {
-						if len(group.tags) == 0 {
-							acc.AddFields(sensorName, group.data, tags, tnow)
 						} else {
-							acc.AddFields(sensorName, group.data, group.tags, tnow)
+							log.Fatalf("E! Failed to read: %v", errRecv)
 						}
 					}
 				}
-				if (errRecv == io.EOF){
-					break
-				}
-				if (errRecv != nil){
-				// Retry only if the retry flag is set to true, else exit with error
-					if m.Retry {
-						continue
-					} else {
-						log.Fatalf("E! Failed to read: %v", errRecv)
-					}
-				}
-			}
-		}(sensor, acc)
+			}(sensor, acc)
+		}
 	}
-
 	return nil
 }
 
